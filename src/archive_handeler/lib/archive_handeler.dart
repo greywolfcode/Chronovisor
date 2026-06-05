797443d763c35de2638a785c5b1697bd0f7d93d2
greywolfcode <229827496+greywolfcode@users.noreply.github.com>
2026-06-05 17:01:37 -0400

    Implement locating chat folder in zip archive

--- src/archive_handeler/lib/archive_handeler.dart
+++ src/archive_handeler/lib/archive_handeler.dart
@@ -1,8 +1,29 @@
-import 'package:archive/archive.dart';
 import 'dart:io';
+
+import 'package:path/path.dart' as p;
+
+import 'package:archive/archive.dart';
+
+final chatPath = p.join('Takeout', 'Google Chat');
 
 void loadExport(String path)
 {
   final bytes = File(path).readAsBytesSync();
   final archive = ZipDecoder().decodeBytes(bytes);
+
+  final List<ArchiveFile> chatFolder = [];
+
+  for (final entry in archive)
+  {
+    //only want folders
+    if (entry.isDirectory)
+    {
+      if (entry.name.contains(chatPath))
+      {
+        chatFolder.add(entry);
+        break;
+      }
+    }    
+  }
+
 }
