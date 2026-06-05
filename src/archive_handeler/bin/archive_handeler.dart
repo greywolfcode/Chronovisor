a0a89de8dc1eafb6934b12f6c875971fddc77727
greywolfcode <229827496+greywolfcode@users.noreply.github.com>
2026-06-05 15:05:30 -0400

    Create archive_handeler package

---
+++ src/archive_handeler/bin/archive_handeler.dart
@@ -0,0 +1,5 @@
+import 'package:archive_handeler/archive_handeler.dart' as archive_handeler;
+
+void main(List<String> arguments) {
+  print('Hello world: ${archive_handeler.calculate()}!');
+}
---
+++ src/archive_handeler/test/archive_handeler_test.dart
@@ -0,0 +1,6 @@
+import 'package:archive_handeler/archive_handeler.dart';
+import 'package:test/test.dart';
+
+void main() {
+  
+}
