d5aa3f054a3174e34b56b907351e2b8985e618ca
greywolfcode <229827496+greywolfcode@users.noreply.github.com>
2026-06-06 17:56:57 -0400

    Prepare stat system

--- src-archive-generator/main.py
+++ src-archive-generator/main.py
@@ -1,12 +1,23 @@
+from enum import Enum
+
 from textual.app import App, ComposeResult
 from textual.widgets import Header
 
+class State(Enum):
+    MAIN_MENU = 0,
+
 class ArchiveGeneratorApp(App):
+
+    def __init__(self):
+        self.state = State.MAIN_MENU
 
     BINDINGS = [("d", "toggle_dark", "Toggle dark mode")]
 
     def compose(self) -> ComposeResult:
         yield Header(name = 'Chronovisor Archive Generator')
+        
+        if self.state == State.MAIN_MENU:
+            pass
 
     def action_toggle_dark(self) -> None:
         """An action to toggle dark mode."""
