65031b24812d23864d641a75e8638f46296bb4cb
greywolfcode <229827496+greywolfcode@users.noreply.github.com>
2026-06-06 15:57:49 -0400

    Create basic layout of Chat theme

--- src/chronovisor/lib/themes.dart
+++ src/chronovisor/lib/themes.dart
@@ -1,30 +1,46 @@
-import 'package:chronovisor/main.dart';
 import 'package:flutter/material.dart';
+
+import 'package:resizable_widget/resizable_widget.dart';
 
 class ChatTheme extends StatelessWidget
 {
-  const ChatTheme({required this.uploadArchive});
+  const ChatTheme({super.key, required this.uploadArchive});
 
   final VoidCallback uploadArchive;
 
   @override
   Widget build(BuildContext context)
   {
-    return Column(
-      children: 
-      [
-        AppBar(
+    return Scaffold(
+      appBar: AppBar(
           title: Text('Chronovisor'),
           actions: [
             IconButton(
-              icon: const Icon(Icons.download),
+              icon: const Icon(Icons.upload),
               onPressed: uploadArchive,
               tooltip: 'Upload Archive',
             ),
           ],
         ),
-
-      ],
+      body: ResizableWidget(
+        children: [
+          Column( //dms & spaces bar
+            children: [
+              const Text('dms & spaces'),
+            ],
+          ),
+          Column( //feed bar
+            children: [
+              Text('feed'),
+            ],
+          ),
+          Column( //selected chat
+            children: [
+              Text('chat'),
+            ],
+            )
+        ],
+      ),
     );
   }
 }
