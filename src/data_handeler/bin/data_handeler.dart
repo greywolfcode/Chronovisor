23d23cae2ebc213fada1e40c048ff7fc1609108f
greywolfcode <229827496+greywolfcode@users.noreply.github.com>
2026-07-08 18:39:47 -0400

    Move data_handeler code to src folder

--- src/data_handeler/bin/data_handeler.dart
+++ src/data_handeler/bin/data_handeler.dart
@@ -1,4 +1,4 @@
-import 'package:data_handeler/data_handeler.dart' as data_handeler;
+import 'package:data_handeler/src/data_handeler.dart' as data_handeler;
 
 void main(List<String> arguments) {
   print('Hello world: ${data_handeler.calculate()}!');
---
+++ src/data_handeler/lib/main.dart
@@ -0,0 +1,3 @@
+library;
+
+export 'src/data_handeler.dart';
