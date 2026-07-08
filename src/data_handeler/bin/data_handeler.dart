b8928e854bd826687da6fdbe039d76ff54192627
greywolfcode <229827496+greywolfcode@users.noreply.github.com>
2026-07-08 19:13:04 -0400

    Create functions for munipulating archives database

--- src/data_handeler/bin/data_handeler.dart
+++ src/data_handeler/bin/data_handeler.dart
@@ -1,5 +1,4 @@
 import 'package:data_handeler/src/data_handeler.dart' as data_handeler;
 
 void main(List<String> arguments) {
-  print('Hello world: ${data_handeler.calculate()}!');
 }
--- src/data_handeler/lib/src/data_handeler.dart
+++ src/data_handeler/lib/src/data_handeler.dart
@@ -1,3 +1,15 @@
-int calculate() {
-  return 6 * 7;
+import 'databases.dart';
+
+final _archiveDatabase = ArchivesDatabase();
+
+void addArchive(String path, String user) async 
+{
+  await _archiveDatabase
+      .into(_archiveDatabase.archivesTable)
+      .insert(ArchivesTableCompanion.insert(path: path, user: user));
+}
+
+Future<List<ArchivesTableData>> getAllArchives() async
+{
+  return await _archiveDatabase.select(_archiveDatabase.archivesTable).get();
 }
