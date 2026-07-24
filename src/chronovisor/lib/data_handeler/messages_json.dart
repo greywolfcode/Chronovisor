ba76ea6dc75ed80bae104dfc869bed5c9d41ef0f
greywolfcode <229827496+greywolfcode@users.noreply.github.com>
2026-07-23 22:22:06 -0400

    Create classes for parsing mesages.json file

---
+++ src/chronovisor/lib/data_handeler/messages_json.dart
@@ -0,0 +1,52 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'messages_json.g.dart';
+
+@JsonSerializable(explicitToJson: true)
+class ChatData {
+  ChatData(this.messages);
+
+  List<Message> messages;
+
+  factory ChatData.fromJson(Map<String, dynamic> json) => _$ChatDataFromJson(json);
+
+  Map<String, dynamic> toJson() => _$ChatDataToJson(this);
+}
+
+
+@JsonSerializable(explicitToJson: true)
+class Message {
+  Message(this.creator, this.text, this.createdDate, this.topicId, this.messageId);
+
+  Creator creator;
+
+  @JsonKey(name: 'created_date')
+  String createdDate;
+
+  String text;
+
+  @JsonKey(name: 'topic_id')
+  String topicId;
+
+  @JsonKey(name: 'message_id')
+  String messageId;
+
+  factory Message.fromJson(Map<String, dynamic> json) => _$MessageFromJson(json);
+
+  Map<String, dynamic> toJson() => _$MessageToJson(this);
+}
+
+@JsonSerializable()
+class Creator {
+  Creator(this.name, this.email, this.userType);
+
+  String name;
+  String email;
+
+  @JsonKey(name: 'user_type')
+  String userType;
+
+  factory Creator.fromJson(Map<String, dynamic> json) => _$CreatorFromJson(json);
+
+  Map<String, dynamic> toJson() => _$CreatorToJson(this);
+}
