35b71829d80247f03f2427adfefb0d4d9f69ffd3
greywolfcode <229827496+greywolfcode@users.noreply.github.com>
2026-07-23 21:34:29 -0400

    Setup basic database for storing messages

--- src/chronovisor/lib/data_handeler/databases.dart
+++ src/chronovisor/lib/data_handeler/databases.dart
@@ -8,6 +8,8 @@ import 'package:drift/native.dart';
 
 part 'databases.g.dart';
 
+
+
 class ArchivesTable extends Table
 {
   IntColumn get id => integer().autoIncrement()();
@@ -26,6 +28,34 @@ class ArchivesDatabase extends _$ArchivesDatabase
   static QueryExecutor _openConnection() 
   {
     final file = File(join('save', 'archives.sqlite'));
+    final absolutePath = file.absolute;
+
+    return NativeDatabase(absolutePath);
+  }
+}
+
+class MessagesTable extends Table
+{
+  IntColumn get id => integer().autoIncrement()();
+  TextColumn get name => text()();
+  TextColumn get email => text()();
+  TextColumn get userType => text()();
+  DateTimeColumn get createdDate => dateTime()();
+  TextColumn get messageText => text()();
+  TextColumn get topicId => text()();
+}
+
+@DriftDatabase(tables: [MessagesTable])
+class MessagesDatabase extends _$ArchivesDatabase 
+{
+  MessagesDatabase(File databasePath, [QueryExecutor? executor]) : super(executor ?? _openConnection(databasePath));
+
+  @override
+  int get schemaVersion => 1;
+
+  static QueryExecutor _openConnection(File databasePath) 
+  {
+    final file = databasePath;
     final absolutePath = file.absolute;
 
     return NativeDatabase(absolutePath);
